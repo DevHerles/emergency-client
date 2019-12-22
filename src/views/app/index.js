0ef2ba27810a40ce240b5a4fcff648e1f80234b2
HerlesINC <herles.incalla@gmail.com>
2019-12-21 22:59:36 -0500

    Reduce menus

--- src/views/app/index.js
+++ src/views/app/index.js
@@ -5,11 +5,6 @@ import { connect } from "react-redux";
 import AppLayout from "../../layout/AppLayout";
 import dashboards from "./dashboards";
 import emergencies from "./emergencies";
-import pages from "./pages";
-import applications from "./applications";
-import ui from "./ui";
-import menu from "./menu";
-import blankPage from "./blank-page";
 
 class App extends Component {
   render() {
@@ -20,12 +15,7 @@ class App extends Component {
         <Switch>
           <Redirect exact from={`${match.url}/`} to={`${match.url}/dashboards`} />
           <Route path={`${match.url}/dashboards`} component={dashboards} />
-          <Route path={`${match.url}/applications`} component={applications} />
           <Route path={`${match.url}/emergencies`} component={emergencies} />
-          <Route path={`${match.url}/pages`} component={pages} />
-          <Route path={`${match.url}/ui`} component={ui} />
-          <Route path={`${match.url}/menu`} component={menu} />
-          <Route path={`${match.url}/blank-page`} component={blankPage} />
           <Redirect to="/error" />
         </Switch>
       </AppLayout>
